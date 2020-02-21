711e60a0cce60f09dea106b05d0437322fc48460
Nor Shiervani <nor.shier97@gmail.com>
2020-02-21 21:01:07 +0100

    Shows background based on city using Pixabay API.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(() => {
     const WEATHERBIT_API_KEY = "732839d858ff424cbd815536e6c31d5d";
+    const PIXABAY_API_KEY = "15315498-9ff1536c360c66ee9d548dde0";
 
     $("#cityinput").on('input', function() {
         this.setAttribute('autocomplete', 'off')
@@ -9,41 +10,62 @@ $(() => {
     });
 
     $("#searchbutton").on('click', function() {
-        searchForecast($("#cityinput").val());
+        let city = $("#cityinput").val();
+        if (city.trim()) {
+            fetchForecastData(city);
+        } else {
+            toggleDataSection(true);
+            updateSearchStatus("Please enter a city.");
+        }
     });
 
-    function searchForecast(city) {
-        var forecasturl = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + city + "&key=" + WEATHERBIT_API_KEY;
+    function fetchForecastData(city) {
+        let forecasturl = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + city + "&key=" + WEATHERBIT_API_KEY;
 
         $.getJSON(forecasturl, function(apidata) {
             if (apidata != null) {
                 displayForecastData(apidata);
             } else {
-                $("#data_section").css("display", "none");
+                toggleDataSection(true);
                 updateSearchStatus('Couldn\'t find weather forecasts for city \"' + city + '\".');
             }
         });
     }
 
     function displayForecastData(apidata) {
-        $("#data_section").empty();
-        $("#data_section").css("display", "initial");
-
+        toggleDataSection(false);
         $.each(apidata.data, function(index) {
-            var date = apidata.data[index].datetime;
-            var desc = apidata.data[index].weather.description + " (" + apidata.data[index].temp + " °C)";
-            var iconurl = 'https://www.weatherbit.io/static/img/icons/' + apidata.data[index].weather.icon + '.png';
+            let date = apidata.data[index].datetime;
+            let desc = apidata.data[index].weather.description + " (" + apidata.data[index].temp + " °C)";
+            let iconurl = 'https://www.weatherbit.io/static/img/icons/' + apidata.data[index].weather.icon + '.png';
 
-            $("#data_section").append(
+            $("#data-section").append(
                 "<div id='dayforecast'><div id='date'>" +
                 date + "</div><div id='description'>" +
                 desc + "<img id='weathericon' src='" +
                 iconurl + "'</div></div>");
         });
-        updateSearchStatus('Showing data for: ' + apidata.city_name + ' (' + apidata.country_code + ')');
+        updateSearchStatus('Showing data for: ' + apidata.city_name + ' (' + apidata.country_code + ')'); +
+        displayCityBackground(apidata.city_name);
     }
 
     function updateSearchStatus(message) {
         $("#searchstatus").text(message);
+    }
+
+    function toggleDataSection(hide) {
+        $("#data-section").css("height", hide ? "0px" : "300px");
+    }
+
+    function displayCityBackground(city) {
+        var URL = "https://pixabay.com/api/?key=" + PIXABAY_API_KEY + "&q=" + city;
+        $.getJSON(URL, function(data) {
+            if (parseInt(data.totalHits) > 0) {
+                let randomindex = Math.floor(Math.random() * (data.hits.length + 1));
+                let imageUrl = data.hits[randomindex].largeImageURL;
+                $("#bg-image").css("background-image", "url(" + imageUrl + ")");
+                $("#bg-color").css("opacity", 0.9);
+            }
+        });
     }
 });
